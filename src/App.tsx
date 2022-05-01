1ae4ba2b55807ccc6eff77319c7861e2a2b5d4ae
dbNaN <damiano.biasioli93@gmail.com>
2022-05-01 19:04:19 +0200

    Implemented dashboard basic design and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,63 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import MenuIcon from "./assets/svg/menu.svg";
+import UsersIcon from "./assets/svg/users.svg";
+import VideoIcon from "./assets/svg/video.svg";
+import ArrowLeftIcon from "./assets/svg/arrow-left.svg";
+import ArrowRightIcon from "./assets/svg/arrow-right.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleMenu = () => {
+    const value = !isOpen;
+    setIsOpen(value);
+    localStorage.setItem("menu", value.toString());
+  };
+
+  const showLabel = (label: string) => {
+    if (isOpen) return <span>{label}</span>;
+  };
+
+  useEffect(() => {
+    //localStorage.getItem("menu");
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="dashboard">
+      <aside className={`sidebar --${isOpen ? "open" : "close"}`}>
+        <div className="sidebar_menu-icon" onClick={toggleMenu}>
+          {isOpen ? (
+            <img
+              src={ArrowLeftIcon}
+              className="sidebar_menu-icon"
+              alt="arrow left"
+            />
+          ) : (
+            <img
+              src={ArrowRightIcon}
+              className="sidebar_menu-icon"
+              alt="arrow right"
+            />
+          )}
+        </div>
+        <ul className="sidebar__list">
+          <li>
+            <img src={UsersIcon} className="sidebar_menu-icon" alt="users" />
+            {showLabel("Users")}
+          </li>
+          <li>
+            <img src={VideoIcon} className="sidebar_menu-icon" alt="video" />
+            {showLabel("Videos")}
+          </li>
+        </ul>
+      </aside>
+      <header className="header">Header</header>
+      <main className="content">Content</main>
+      <footer className="footer">Footer</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
